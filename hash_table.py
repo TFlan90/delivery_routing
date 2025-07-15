99caf34d9214d0d8623c4b7288175be8c062bd67
TFlan90 <Tdflan90@gmail.com>
2025-07-15 12:33:14 -0700

    added docstrings for package and hash_table files

--- hash_table.py
+++ hash_table.py
@@ -1,7 +1,9 @@
 
 class HashTable:
+    """simple hash table implementation"""
 
     def __init__(self, capacity=10):
+        """hash table is a list of lists(buckets)"""
         self.hash_table = []
         for i in range(capacity):
             self.hash_table.append([])
@@ -10,24 +12,33 @@ class HashTable:
         key = key % 10
         return key
 
+
     def insert(self, package):
+        """compute key using package_id, append entire package to bucket"""
         key = self.hash_calculation(package.package_id)
         self.hash_table[key].append(package)
 
+
     def lookup(self, package_id):
+        """compute key given package_id, iterate over bucket, return package if found else None"""
         key = self.hash_calculation(package_id)
         for pkg in self.hash_table[key]:
             if pkg.package_id == package_id:
                 return pkg
         return None
 
+
     def remove(self, package_id):
+        """"
+        compute key given package_id
+        iterate over bucket, return True if package found and deleted else False
+        unrequired, but it felt like something that I should add
+        """
         key = self.hash_calculation(package_id)
         for i, p in enumerate(self.hash_table[key]):
             if p.package_id == package_id:
                 self.hash_table[key].pop(i)
                 return True
-
         return False
 
 
@@ -36,16 +47,3 @@ class HashTable:
 
     def __repr__(self):
       return str(self.hash_table)
-
-
-
-
-
-
-
-
-
-
-
-
-
--- package.py
+++ package.py
@@ -1,5 +1,5 @@
 from hash_table import HashTable
-
+import csv
 
 class Package:
     def __init__(self, package_id: int,
@@ -7,6 +7,7 @@ class Package:
                  delivery_city : str,
                  state : str,
                  delivery_zip: int,
+                 delivery_deadline: str,
                  package_weight : int,
                  notes : str,
                  delivery_status : str,
@@ -17,6 +18,7 @@ class Package:
         self.delivery_address = delivery_address
         self.delivery_city = delivery_city
         self.state = state
+        self.deliver_deadline = delivery_deadline
         self.delivery_zip = delivery_zip
         self.package_weight = package_weight
         self.notes = notes
@@ -29,52 +31,31 @@ class Package:
         return ("package_id: {},"
                 "delivery_address: {},"
                 "delivery_city: {},"
-                "state: {},"
                 "delivery_zip: {},"
+                "delivery_deadline: {},"
                 "package_weight: {},"
                 "notes: {},"
                 "delivery_status: {},"
                 "loading_time: {},"
-                "delivery_time: {}".format(self.package_id, self.delivery_address, self.delivery_city, self.state, self.delivery_zip,
-                                           self.package_weight, self.notes, self.delivery_status, self.loading_time, self.delivery_time))
+                "delivery_time: {}".format(self.package_id, self.delivery_address, self.delivery_city, self.delivery_zip,
+                                           self.deliver_deadline, self.package_weight, self.notes, self.delivery_status,
+                                           self.loading_time, self.delivery_time))
 
 
     def __str__(self):
         return ("package_id: {},"
                 "delivery_address: {},"
                 "delivery_city: {},"
-                "state: {},"
                 "delivery_zip: {},"
+                "delivery_deadline: {},"
                 "package_weight: {},"
                 "notes: {},"
                 "delivery_status: {},"
                 "loading_time: {},"
-                "delivery_time: {}".format(self.package_id, self.delivery_address, self.delivery_city, self.state,
-                                           self.delivery_zip,
-                                           self.package_weight, self.notes, self.delivery_status, self.loading_time,
-                                           self.delivery_time))
+                "delivery_time: {}".format(self.package_id, self.delivery_address, self.delivery_city, self.delivery_zip,
+                                           self.deliver_deadline, self.package_weight, self.notes, self.delivery_status,
+                                           self.loading_time, self.delivery_time))
 
-p1 = Package(1, "195 W Oakland Ave","Salt Lake City","UT",84115,
-                   21 ,"", "delivered", "10:30 AM",  "2:20 PM")
-p20 = Package(20, "123 Test Ave","SLC","KY",84115,
-                   21 ,"", "delivered", "10:30 AM",  "2:20 PM")
 
-my_table = HashTable()
-my_table.insert(p1)
-my_table.insert(p20)
-print("Hash Table after insertions:")
-print(my_table)
-print("look-up: p1 ")
-print(my_table.lookup(p1.package_id))
-print("Look-up: p20 ")
-print(my_table.lookup(p20.package_id))
-my_table.remove(p20.package_id)
 
-print("Hash Table after deletion(s): 20")
-print(my_table)
-
-my_table.remove(p1.package_id)
-print("Hash Table after deletion(s): 1")
-print(my_table)
-my_table.remove(p20.package_id)
 
